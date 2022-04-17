63130302f4e3776e2add7818601fc8b6516824a4
junha <goodjunha@gmail.com>
2022-04-17 16:44:28 +0900

    ADD:: 변수 활용

--- my name.playground/Contents.swift
+++ my name.playground/Contents.swift
@@ -1,4 +1,21 @@
-import Foundation
-let name:String = "yagom"
-print("MY name is \(name)")  //변수나 상수를 집어 넣을 때 c언어는 %d를 쓰지만 swift는 \(변수,상수)를 사용한댜.
+//import Foundation
+//let name:String = "yagom"
+//print("MY name is \(name)")  //변수나 상수를 집어 넣을 때 c언어는 %d를 쓰지만 swift는 \(변수,상수)를 사용한다
 
+//변수
+//var name : String = "yagom"
+//var age: Int = 100
+//var job = "IOS Programmer"
+//var height = 181.5
+////print("\(type(of: ))") = 변수 타입을 확인 할 수 있다
+//age = 99
+//job = "Writer"
+//
+//print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 직업은 \(job)입니다. 비밀이지만, 키는 \(height)센티미터입니다.")
+
+//변수 활용
+var name : String = "junha park"
+var age: Int = 17;
+var job = "student"
+var higth = 171.3;
+print("저의 이름은 \(name)입니다, 그리고 저의 나이는 \(age)이며 저의 신분은 \(job)입니다. 정말 비밀이지만 말씀드리면 저의 키는 \(higth)입니다")
