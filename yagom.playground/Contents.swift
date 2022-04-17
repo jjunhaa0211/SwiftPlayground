921f13dec65a0d063cc239862053a962032eb534
junha <goodjunha@gmail.com>
2022-04-17 18:09:15 +0900

    ADD::float and double

--- yagom.playground/Contents.swift
+++ yagom.playground/Contents.swift
@@ -2,6 +2,8 @@
 //let name:String = "yagom"
 //print("MY name is \(name)")  //변수나 상수를 집어 넣을 때 c언어는 %d를 쓰지만 swift는 \(변수,상수)를 사용한다
 
+import Darwin
+
 //변수
 //var name : String = "yagom"
 //var age: Int = 100
@@ -33,15 +35,34 @@
 
 //Int 와 UInt
 
-var integer: Int = -100
-let unsignedIntteger: UInt = 50   // 음수 불가능 = UInt
-print("integer 값: \(integer),unsigndInteger 값: \(unsignedIntteger)")
-print("Int 최댓값: \(Int.max), Int 최솟값: \(Int.min)")
-print("Int 최댓값: \(UInt.max), Int 최솟값: \(UInt.min)")
-let largeInteger: Int64 = Int64.max
-let smallUnsignedInteger: UInt8 = UInt8.max
-print("Int64 최댓값: \(largeInteger), UInt8 최댓값: \(smallUnsignedInteger)")
+//var integer: Int = -100
+//let unsignedIntteger: UInt = 50   // 음수 불가능 = UInt
+//print("integer 값: \(integer),unsigndInteger 값: \(unsignedIntteger)")
+//print("Int 최댓값: \(Int.max), Int 최솟값: \(Int.min)")
+//print("Int 최댓값: \(UInt.max), Int 최솟값: \(UInt.min)")
+//let largeInteger: Int64 = Int64.max
+//let smallUnsignedInteger: UInt8 = UInt8.max
+//print("Int64 최댓값: \(largeInteger), UInt8 최댓값: \(smallUnsignedInteger)")
 
 //let tooLarge: Int = Int.max + 1
 //let cannotBeNegetive: UInt = -5
 
+//var boolean: Bool = true
+//boolean.toggle()  // true - false 반전
+//let iLoveYou: Bool = true
+//let isTimeUnlimited: Bool = false
+//print("시간은 무한합니까?: \(isTimeUnlimited)")
+
+
+
+//float and double
+
+var floatValue: Float = 1234567890.1
+
+let doubleValue: Double = 1234567890.1
+
+print("floatValue : \(floatValue) doubleValue : \(doubleValue)")
+
+floatValue = 123456.1
+
+print(floatValue)
