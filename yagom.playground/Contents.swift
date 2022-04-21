096eaa31bba182cd35cdf8732489fbfcbbe98c55
junha <goodjunha@gmail.com>
2022-04-21 19:44:11 +0900

    ADD::Value swifch

--- yagom.playground/Contents.swift
+++ yagom.playground/Contents.swift
@@ -316,21 +316,32 @@
 //    print("아무 것도 아님")
 //}
 
-let integerValue: Int = 5
+//let integerValue: Int = 5
+//
+//switch integerValue {
+//case 1:
+//    print("Value == zero")
+//
+//case 1...10:
+//    print("Value == 1~10")
+//    fallthrough
+//
+//case Int.min..<0, 101..<Int.max:
+//    print("Value < 0 or Value > 100")
+//    break
+//
+//default:
+//    print("10 < Value <= 100")
+//
+//}
 
-switch integerValue {
-case 1:
+let doubleValue: Double = 3.0
+
+switch doubleValue {
+case 0:
     print("Value == zero")
-    
-case 1...10:
-    print("Value == 1~10")
-    fallthrough
-    
-case Int.min..<0, 101..<Int.max:
-    print("Value < 0 or Value > 100")
-    break
-    
+case 1.5...10.5:
+    print("1.5 <= Value <= 10.5")
 default:
-    print("10 < Value <= 100")
-    
+    print("Value == \(doubleValue)")
 }
