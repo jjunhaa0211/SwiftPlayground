2ece726366291c667ac101d66b762656ac489732
junha <goodjunha@gmail.com>
2022-05-07 21:12:37 +0900

    ADD::print(), dump() review

--- yagom.playground/Contents.swift
+++ yagom.playground/Contents.swift
@@ -334,14 +334,36 @@
 //    print("10 < Value <= 100")
 //
 //}
-
-let doubleValue: Double = 3.0
-
-switch doubleValue {
-case 0:
-    print("Value == zero")
-case 1.5...10.5:
-    print("1.5 <= Value <= 10.5")
-default:
-    print("Value == \(doubleValue)")
+//
+//let doubleValue: Double = 3.0
+//
+//switch doubleValue {
+//case 0:
+//    print("Value == zero")
+//case 1.5...10.5:
+//    print("1.5 <= Value <= 10.5")
+//default:
+//    print("Value == \(doubleValue)")
+//}
+struct BasicInformation{
+    let name: String
+    var age: Int
 }
+var yagomInfo = BasicInformation(name: "yagom",age: 99)
+
+class person {
+    var height: Float = 0.0
+    var weight: Float = 0.0
+}
+
+let yagom: person = person()
+yagom.height = 182.5
+yagom.weight = 78.5
+
+print(yagomInfo)
+dump(yagomInfo)
+
+print(yagom)
+
+dump(yagom)
+
