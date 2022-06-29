1e3fb2f6bd7778f1a870a9ba61363138838864ca
junha <goodjunha@gmail.com>
2022-06-29 14:57:19 +0900

    ADD::for

--- Fest Campus.playground/Contents.swift
+++ Fest Campus.playground/Contents.swift
@@ -2,10 +2,8 @@ import UIKit
 
 //var nume = 5
 //nume = 1
-
 //var name = "Jason"
 //name = "Jay"
-
 //let quote = "I'm mather father genius, man"
 //
 //let num = 5
@@ -14,14 +12,12 @@ import UIKit
 //
 //let halfOfNum = otherNum / 2
 //
-
 //let num = 360
 //
 //print(num.isMultiple(of: 3))
 //
 //let random = Int.random(in: 0...300)
 //
-
 //let score = 3.6
 //let isMan = true
 //let isHuman = false
@@ -29,13 +25,11 @@ import UIKit
 //var isSingle = true
 //isSingle.toggle()// toggle = true면 false가 되고 false면 true가 된다
 //isSingle
-
 //let difficulgy = "쉽다"
 //let maximumAge = 80
 //
 //let message =  "\(maximumAge) 할머니도 배우는 IOS 개발은 \(difficulgy)"
 //
-
 //let ages = [3, 20, 60]
 //let colors = ["green", "red", "yellow"]
 //
@@ -48,6 +42,84 @@ import UIKit
 //]
 //
 //let code = langugeCode["미국"]
+//var emptyArr: [Int] = [1,2]
+//var emptyDic: [String: Any] = [:]
+//enum WeekDay {
+//    case mon
+//    case tur
+//    case wed
+//    case thu
+//    case fri
+//}
+//
+//var today: WeekDay = .mon
+//enum MediaType {
+//    case audio(String)
+//    case video(String)
+//}
+//
+//var mp3: MediaType = .audio("mp3")
+//var h264: MediaType = .video("h264")
+//
+//let age = 10
+//
+//if age > 20 {
+//    print("성인입니다")
+//}else{
+//    print("미성년입니다")
+//}
+//
+//if age >= 10 && age < 20 {
+//    print("10대 입니다")
+//}else if age > 20 && age < 30 {
+//    print("20대 입니다")
+//}else if age > 20 && age < 30 {
+//    print("30대 입니다")
+//}else if age > 20 && age < 30 {
+//    print("40대 입니다")
+//}else {
+//    print("......")
+//}
+//enum Weather {
+//    case sun
+//    case cloud
+//    case rain
+//}
+//
+//var weather: Weather = .sun
+//
+//switch weather {
+//case .sun:
+//    print("맑아여")
+//case .cloud:
+//    print("흐려요")
+//case .rain:
+//    print("비와요")
+//}
+//let ages = [3, 20, 60]
+//let languageCode = [
+//    "한국" : "ko",
+//    "미국" : "en",
+//    "일본" : "ja",
+//]
+//
+//for age in ages {
+//    print("age : \(age)")
+//}
+//
+//for (key,value) in languageCode {
+//    print("\(key)의 언어코드는 \(value)")
+//}
+//for _ in 0..<10 {
+//    print("나는 IOS개발자다")
+//}
+//var count = 10
+//
+//print("Ready!")
+//
+//while count > 0 {
+//    print("\(count)...")
+//}
+//
+//print("START!")
 
-var emptyArr: [Int] = [1,2]
-var emptyDic: [String: Any] = [:]
