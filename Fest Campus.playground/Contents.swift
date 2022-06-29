ea4e6c59598cc31062faf4fd6c6e26261cc15af1
junha <goodjunha@gmail.com>
2022-06-29 13:32:55 +0900

    ADD:: let, var

---
+++ Fest Campus.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+//var nume = 5
+//nume = 1
+
+//var name = "Jason"
+//name = "Jay"
+
+//let quote = "I'm mather father genius, man"
+//
+//let num = 5
+//
+//let otherNum = num + 2
+//
+//let halfOfNum = otherNum / 2
+//
+
+//let num = 360
+//
+//print(num.isMultiple(of: 3))
+//
+//let random = Int.random(in: 0...300)
+//
+
+let score = 3.6
