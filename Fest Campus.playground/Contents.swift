0723aad363620130266242286044da4dbd108fb9
junha <goodjunha@gmail.com>
2022-06-29 14:08:47 +0900

    ADD::arr

--- Fest Campus.playground/Contents.swift
+++ Fest Campus.playground/Contents.swift
@@ -22,4 +22,32 @@ import UIKit
 //let random = Int.random(in: 0...300)
 //
 
-let score = 3.6
+//let score = 3.6
+//let isMan = true
+//let isHuman = false
+//
+//var isSingle = true
+//isSingle.toggle()// toggle = true면 false가 되고 false면 true가 된다
+//isSingle
+
+//let difficulgy = "쉽다"
+//let maximumAge = 80
+//
+//let message =  "\(maximumAge) 할머니도 배우는 IOS 개발은 \(difficulgy)"
+//
+
+//let ages = [3, 20, 60]
+//let colors = ["green", "red", "yellow"]
+//
+//let color = colors[0]
+//
+//let langugeCode = [
+//    "한국" : "ko",
+//    "미국" : "en",
+//    "일본" : "ja",
+//]
+//
+//let code = langugeCode["미국"]
+
+var emptyArr: [Int] = [1,2]
+var emptyDic: [String: Any] = [:]
